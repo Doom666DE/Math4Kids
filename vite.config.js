dbca98ae1a70a2f6439d4c3bdc1f754e46f139ad
Doom666DE <Doom666DE@users.noreply.github.com>
2026-05-10 20:23:02 +0200

    Fix GitHub Pages workflow

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Math4Kids/",
+  base: process.env.BASE_PATH ?? "/Math4Kids/",
   plugins: [react()],
 });
